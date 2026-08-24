cbf12cfdc81ea1120452b90262cf926496a3b063
NullCM <141198826+NullCM@users.noreply.github.com>
2026-08-24 22:35:22 +0800

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -695,7 +695,7 @@ SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 {
 #ifdef CONFIG_KSU_SUSFS
 	if (ksu_handle_setresuid(ruid, euid, suid)) {
-		pr_info("Something wrong with ksu_handle_setresuid()\n");
+		pr_info("Something wrong with ksu_handle_setresuid()\\n");
 	}
 #endif
 	return __sys_setresuid(ruid, euid, suid);
