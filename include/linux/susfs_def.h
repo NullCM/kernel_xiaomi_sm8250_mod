9b03a9badea2e8074723c354728f6fac86b08f7d
NullCM <141198826+NullCM@users.noreply.github.com>
2026-08-26 10:09:21 +0800

    Update susfs_def.h

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -149,31 +149,31 @@ struct fsnotify_mark *vfsmount_mark, u32 mask, void *data,    \
 int data_type, susfs_fname_t file_name, u32 cookie)
 #endif
 
-extern bool susfs_is_current_proc_umounted(void) {
+bool susfs_is_current_proc_umounted(void) {
 	return (likely(test_thread_flag(TIF_PROC_UMOUNTED)));
 }
 
-extern void susfs_set_current_proc_umounted(void) {
+void susfs_set_current_proc_umounted(void) {
 	set_thread_flag(TIF_PROC_UMOUNTED);
 }
 
-extern void susfs_clear_current_proc_umounted(void) {
+void susfs_clear_current_proc_umounted(void) {
 	clear_thread_flag(TIF_PROC_UMOUNTED);
 }
 
-extern bool susfs_is_current_proc_umounted_for_zygote_next(void) {
+bool susfs_is_current_proc_umounted_for_zygote_next(void) {
 	return (likely(test_thread_flag(TIF_PROC_UMOUNTED_FOR_ZYGOTE_NEXT)));
 }
 
-extern void susfs_set_current_proc_umounted_for_zygote_next(void) {
+void susfs_set_current_proc_umounted_for_zygote_next(void) {
 	set_thread_flag(TIF_PROC_UMOUNTED_FOR_ZYGOTE_NEXT);
 }
 
-extern void susfs_clear_current_proc_umounted_for_zygote_next(void) {
+void susfs_clear_current_proc_umounted_for_zygote_next(void) {
 	clear_thread_flag(TIF_PROC_UMOUNTED_FOR_ZYGOTE_NEXT);
 }
 
-extern bool susfs_is_current_proc_umounted_app(void) {
+bool susfs_is_current_proc_umounted_app(void) {
 	return (likely(test_thread_flag(TIF_PROC_UMOUNTED)) &&
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
 			__kuid_val(current_uid()) >= 10000);
@@ -182,15 +182,15 @@ extern bool susfs_is_current_proc_umounted_app(void) {
 #endif
 }
 
-extern bool susfs_is_current_proc_no_su(void) {
+bool susfs_is_current_proc_no_su(void) {
 	return (likely(test_thread_flag(TIF_PROC_NO_SU)));
 }
 
-extern void susfs_set_current_proc_no_su(void) {
+void susfs_set_current_proc_no_su(void) {
 	set_thread_flag(TIF_PROC_NO_SU);
 }
 
-extern void susfs_clear_current_proc_no_su(void) {
+void susfs_clear_current_proc_no_su(void) {
 	clear_thread_flag(TIF_PROC_NO_SU);
 }
 
